49697bf1d6bc82ec631036cc0ac09e0b72534b5d
jacky.leung <jacky.leung@oocl.com>
2018-11-26 15:13:52 +0800

    clean police checkDriver duplicated return

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -3,7 +3,6 @@ package com.tws.refactoring;
 public class Police {
     private int adultAge = 18;
     public boolean checkDriver(Driver driver) {
-        if(driver.getAge() >= adultAge) return true;
-        else return false;
+        return (driver.getAge() >= adultAge);
     }
 }
