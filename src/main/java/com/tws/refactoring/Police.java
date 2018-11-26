20410b44675eef5653521f233791c15c421bad3d
jacky.leung <jacky.leung@oocl.com>
2018-11-26 15:12:24 +0800

    police add adult age

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,9 @@
 package com.tws.refactoring;
 
 public class Police {
+    private int adultAge = 18;
     public boolean checkDriver(Driver driver) {
-        if(driver.getAge() >= 18) return true;
+        if(driver.getAge() >= adultAge) return true;
         else return false;
     }
 }
